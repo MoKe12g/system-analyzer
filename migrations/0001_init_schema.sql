2105683c9047dcdcbebcccd586689a821d1049a7
Moritz Kempe <moritzkempe@mailbox.org>
2025-08-17 12:10:01 +0200

    removed non database related, polished code

--- migrations/0001_init_schema.sql
+++ migrations/0001_init_schema.sql
@@ -1,8 +1,7 @@
 create table dpkg_packages
 (
-    package_name   TEXT
-        constraint dpkg_packages_pk
-            primary key not null,
+    package_name TEXT
+        constraint dpkg_packages_pk primary key not null,
     version        TEXT not null,
     date_installed datetime not null
 );
@@ -10,14 +9,9 @@ create table dpkg_packages
 create table files
 (
     path       TEXT
-        constraint files_pk
-            primary key
-        not null,
-    size       integer
-        not null,
-    is_folder  boolean
-        not null,
+        constraint files_pk primary key not null,
+    size       integer                  not null,
+    is_folder  boolean                  not null,
     package    TEXT,
-    is_changed boolean
-        not null
+    is_changed boolean                  not null
 );
--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,28 @@
-use clap::Parser;
 use sqlx::sqlite::SqlitePoolOptions;
-
-#[derive(Parser)]
-#[command(version, about, long_about = None)]
-struct Cli {
-    /// Turn debugging information on
-    #[arg(short, long, action = clap::ArgAction::Count)]
-    debug: u8,
-}
+use sqlx::{Pool, Sqlite};
 
 #[tokio::main]
 async fn main() {
-    println!("Hello, world!");
-
-    // use clap to parse arguments
-    let args = Cli::parse();
-
     // ?mode=rwc does create the database if it doesn't exist
     // https://stackoverflow.com/questions/72763578/how-to-create-a-sqlite-database-with-rust-sqlx
-    let database_url = "sqlite://data.sqlite?mode=rwc";
-    let root_dir = "/";
-    let excluded_dirs = ["tmp", "home", "proc", "dev", "sys"];
+    let _database_url = "sqlite://database.sqlite";
+    let _root_dir = "/";
+    let _excluded_dirs = ["tmp", "home", "proc", "dev", "sys"];
 
+    let database = create_database_connection(_database_url).await;
+
+    // Test insertion as an example for using the database
+    let _insertion_result = sqlx::query!("INSERT INTO dpkg_packages (package_name, version, date_installed) VALUES ('system-analyzer', '0.1a', NULL)")
+        .execute(&database);
+}
+
+async fn create_database_connection(database_url: &str) -> Pool<Sqlite> {
     let database = SqlitePoolOptions::new()
         .max_connections(5)
-        .connect(&database_url).await.expect("Wasn't able to connect or create the database using the given database_url");
+        .connect(format!("{}{}", &database_url, "?mode=rwc").as_str())
+        .await
+        .expect("Wasn't able to connect or create the database using the given database_url");
 
-    // Run migrations (create tables, seed data, etc.)
-    // 1) cargo install sqlx-cli --no-default-features --features rustls,sqlite
-    // 2) sqlx migrate add init
-    // 3) put your SQL files under migrations
-    let migration_result = sqlx::migrate!().run(&database).await;
-    // TODO: Create example data in tables
+    sqlx::migrate!().run(&database).await.expect("Wasn't able to migrate the database");
+    database
 }
