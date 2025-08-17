b9bd43de66581458406b8c34a97fe86d89c927b5
Moritz Kempe <moritzkempe@mailbox.org>
2025-08-17 02:17:48 +0200

    improved migration and upgraded packages

---
+++ migrations/0001_init_schema.sql
@@ -0,0 +1,23 @@
+create table dpkg_packages
+(
+    package_name   TEXT
+        constraint dpkg_packages_pk
+            primary key not null,
+    version        TEXT not null,
+    date_installed datetime not null
+);
+
+create table files
+(
+    path       TEXT
+        constraint files_pk
+            primary key
+        not null,
+    size       integer
+        not null,
+    is_folder  boolean
+        not null,
+    package    TEXT,
+    is_changed boolean
+        not null
+);
