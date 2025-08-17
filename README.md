68afb5ad85fcc9f39d31aa233341ef0ffd915edd
Moritz Kempe <moritzkempe@mailbox.org>
2025-08-17 19:08:46 +0200

    fixed filesize, introduced option for packages and is_changed

--- README.md
+++ README.md
@@ -5,3 +5,15 @@
 Use `DATABASE_URL="sqlite://database.sqlite?mode=rwc" cargo sqlx prepare` for preparing sqlx cache.
 
 You need to have sqlx-cli installed.
+
+## Benchmarks
+
+Walk through a minimal cdebootstrap installed debian bookworm
+
+```
+quantenregen@quantenregen-home-i37100:~/Schreibtisch/system-analyzer$ time ./target/release/system-analyzer
+
+real    1m22,840s
+user    0m3,833s
+sys     0m4,088s
+```
--- src/crawler.rs
+++ src/crawler.rs
@@ -4,41 +4,38 @@ use sqlx::Error;
 use sqlx::Pool;
 use sqlx::Sqlite;
 use std::fs::{FileType, Metadata};
-use std::os::unix::fs::MetadataExt;
 use tokio::fs;
 use tokio::fs::{DirEntry, ReadDir};
 
 pub struct File
 {
     path: String,
-    size: u32,
+    size: i64,
     is_folder: bool,
-    package: String,
-    is_changed: bool,
+    package: Option<String>,
+    is_changed: Option<bool>,
 }
 
 impl File {
-    async fn from_read_dir(input_file: &DirEntry, input_file_type: &FileType, input_file_metadata: &Metadata) -> Self {
+    fn from_read_dir(input_file: &DirEntry, input_file_type: &FileType, input_file_metadata: &Metadata) -> Self {
         File {
             path: input_file.path().display().to_string(),
-            size: input_file_metadata.size() as u32,
+            size: input_file_metadata.len() as i64,
             is_folder: input_file_type.is_dir(),
-            package: "".to_string(),
-            is_changed: true,
+            package: None,
+            is_changed: None,
         }
     }
-}
 
-impl File {
-    pub fn new(path: String, size: u64, is_folder: bool, package: String, is_changed: bool) -> Self {
-        File { path, size: size as u32, is_folder, package, is_changed }
+    pub fn new(path: String, size: u64, is_folder: bool, package: Option<String>, is_changed: Option<bool>) -> Self {
+        File { path, size: size as i64, is_folder, package, is_changed }
     }
 
     pub fn get_path(&self) -> &String {
         &self.path
     }
 
-    pub fn get_size(&self) -> &u32 {
+    pub fn get_size(&self) -> &i64 {
         &self.size
     }
 
@@ -46,11 +43,11 @@ impl File {
         &self.is_folder
     }
 
-    pub fn get_package(&self) -> &String {
+    pub fn get_package(&self) -> &Option<String> {
         &self.package
     }
 
-    pub fn is_changed(&self) -> &bool {
+    pub fn is_changed(&self) -> &Option<bool> {
         &self.is_changed
     }
 }
@@ -76,10 +73,10 @@ pub(crate) async fn crawl(input_file: &DirEntry, database: &Pool<Sqlite>) -> Res
     let file_type = input_file.file_type().await?;
     let file_metadata = input_file.metadata().await?;
     // create entry in database
-    let file = File::from_read_dir(input_file, &file_type, &file_metadata).await;
-    sqlx::query!("INSERT INTO files (path, size, is_folder, package, is_changed) VALUES (?, ?, ?, NULL, NULL);",
-    file.path, file.size, file.is_folder)
-        .execute(database).await?; // TODO: Is it wise to dereference here?
+    let file = File::from_read_dir(input_file, &file_type, &file_metadata);
+    sqlx::query!("INSERT INTO files (path, size, is_folder, package, is_changed) VALUES (?, ?, ?, ?, ?);",
+    file.path, file.size, file.is_folder, file.package, file.is_changed)
+        .execute(database).await?;
     debug!("Added {} to the database", file.path);
     // TODO: Improve performance using https://patrickfreed.github.io/rust/2021/10/15/making-slow-rust-code-fast.html
     // continue crawl
