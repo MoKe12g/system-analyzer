86cc7c7e52d9d55aa5dbd90244a5ec2f8b0d0dde
Moritz Kempe <moritzkempe@mailbox.org>
2025-08-17 20:52:10 +0200

    add file_crawler (#2)
    * added get_files_from_directory function
    
    * first version of crawler
    
    * fixed directory exclusion, added errors to crawler
    
    * fixed filesize, introduced option for packages and is_changed
    
    * added anyhow for the main function, improved get_files_from_directory loop
    
    * made existing database readonly, improved struct datatypes, made println into warnings, added database path builder
    
    * database migration read-only check, fixed metadata read crash when file is not found, added symlink check in crawler
    
    * improved symlink handling in crawler, variable names, added fs feature for tokio

---
+++ src/crawler.rs
@@ -0,0 +1,97 @@
+use async_recursion::async_recursion;
+use log::{debug, warn};
+use sqlx::Error;
+use sqlx::Pool;
+use sqlx::Sqlite;
+use std::fs::{FileType, Metadata};
+use tokio::fs;
+use tokio::fs::{DirEntry, ReadDir};
+
+#[derive(Debug, Clone)]
+pub struct File
+{
+    path: String,
+    size: i64,
+    is_folder: bool,
+    package: Option<String>,
+    is_changed: Option<bool>,
+}
+
+impl File {
+    fn from_read_dir(input_file: &DirEntry, input_file_type: &FileType, input_file_metadata: &Metadata) -> Self {
+        File {
+            path: input_file.path().display().to_string(),
+            size: input_file_metadata.len() as i64,
+            is_folder: input_file_type.is_dir(),
+            package: None,
+            is_changed: None,
+        }
+    }
+
+    pub fn new(path: String, size: i64, is_folder: bool, package: Option<String>, is_changed: Option<bool>) -> Self {
+        File { path, size, is_folder, package, is_changed }
+    }
+
+    pub fn get_path(&self) -> &String {
+        &self.path
+    }
+
+    pub fn get_size(&self) -> i64 {
+        self.size
+    }
+
+    pub fn is_folder(&self) -> bool {
+        self.is_folder
+    }
+
+    pub fn get_package(&self) -> &Option<String> {
+        &self.package
+    }
+
+    pub fn is_changed(&self) -> &Option<bool> {
+        &self.is_changed
+    }
+}
+
+pub async fn get_files_from_directory(mut dir: ReadDir) -> std::io::Result<Vec<DirEntry>> {
+    // get files in directory
+    let mut files: Vec<DirEntry> = Vec::new();
+    while let Some(entry) = dir.next_entry().await? {
+        files.push(entry);
+    }
+    Ok(files)
+}
+
+#[async_recursion]
+pub(crate) async fn crawl(input_file: &DirEntry, database: &Pool<Sqlite>) -> Result<(), Error> {
+    let file_type = input_file.file_type().await?;
+    let file_metadata = input_file.metadata().await?;
+    // create entry in database
+    let file = File::from_read_dir(input_file, &file_type, &file_metadata);
+    if file_type.is_symlink() {
+        return Ok(())
+    }
+    sqlx::query!("INSERT INTO files (path, size, is_folder, package, is_changed) VALUES (?, ?, ?, ?, ?);",
+        file.path, file.size, file.is_folder, file.package, file.is_changed)
+        .execute(database).await?;
+    debug!("Added {} to the database", file.path);
+    // TODO: Improve performance using https://patrickfreed.github.io/rust/2021/10/15/making-slow-rust-code-fast.html
+    // continue crawl
+    if file_type.is_dir() {
+        match fs::read_dir(input_file.path()).await {
+            Ok(folder) => {
+                let files = get_files_from_directory(folder).await;
+                match files {
+                    Ok(files) => {
+                        for entry in &files {
+                            crawl(entry, database).await?;
+                        }
+                    },
+                    Err(err) => { warn!("Skipped files in Folder {} because error occurred. {:?}", file.path, err); }
+                }
+            },
+            Err(err) => { warn!("Skipped {} because error occurred. {:?}", file.path, err); }
+        }
+    }
+    Ok(())
+}
--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,44 @@
+mod crawler;
+
+use log::info;
 use sqlx::sqlite::{SqliteConnectOptions, SqliteJournalMode, SqlitePoolOptions};
 use sqlx::{Pool, Sqlite};
 use std::str::FromStr;
+use tokio::fs;
 
 #[tokio::main]
-async fn main() -> Result<(), sqlx::Error> {
-    let _database_url = "sqlite://database.sqlite";
-    let _root_dir = "/";
-    let _excluded_dirs = ["tmp", "home", "proc", "dev", "sys"];
+async fn main() -> anyhow::Result<()> {
+    let database_filepath = "database.sqlite";
+    let _database_url = format!("{}{}", "sqlite://",database_filepath);
+    // Cannot crawl if database exists. Because it would make both crawl results useless in the process.
+    let database_exists = match fs::metadata(database_filepath).await {
+        Ok(metadata) => metadata.is_file(), // because a sqlite database is not a directory
+        Err(_) => false,
+    };
+    if database_exists { info!("Database is already populated, therefore it will be read-only.") }
+    let root_dir_str = "/home/quantenregen/Schreibtisch/test-bookworm/";
+    let excluded_dirs = ["tmp", "home", "proc", "dev", "sys"];
 
-    let database = create_database_connection(_database_url).await?;
+    let database = create_database_connection(_database_url, database_exists).await?;
+
+    let root_dir = fs::read_dir(root_dir_str).await?;
+    let files = crawler::get_files_from_directory(root_dir).await?;
+
+    // filter virtual directories
+    // filters only at the top level because there shouldn't be more virtual directories further down
+    let filtered_files = files.iter()
+        .filter(|entry|
+            {
+                !excluded_dirs
+                    .contains(&entry.file_name().to_string_lossy().as_ref())
+            })
+        .collect::<Vec<_>>();
+
+
+    // crawl the file system for files and directories
+    for file in filtered_files {
+        crawler::crawl(file, &database).await?;
+    }
 
     // Test insertion as an example for using the database
     sqlx::query!("INSERT OR IGNORE INTO dpkg_packages (package_name, version, date_installed) VALUES (?, ?, CURRENT_TIMESTAMP)",
@@ -17,17 +47,20 @@ async fn main() -> Result<(), sqlx::Error> {
     Ok(())
 }
 
-async fn create_database_connection(database_url: &str) -> Result<Pool<Sqlite>, sqlx::Error> {
-    let sqlite_options = SqliteConnectOptions::from_str(database_url)?
+async fn create_database_connection(database_url: String, read_only: bool) -> Result<Pool<Sqlite>, sqlx::Error> {
+    let sqlite_options = SqliteConnectOptions::from_str(&database_url)?
         .foreign_keys(true)
         .create_if_missing(true)
+        .read_only(read_only)
         .journal_mode(SqliteJournalMode::Wal);
 
     let database = SqlitePoolOptions::new()
-        .max_connections(5)
+        .max_connections(10)
         .connect_with(sqlite_options)
         .await?;
 
-    sqlx::migrate!().run(&database).await?;
+    if !read_only {
+        sqlx::migrate!().run(&database).await?;
+    }
     Ok(database)
 }
